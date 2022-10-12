7d71dc37ddcbb7def2d6f7d8d08b382bfa1a40ca
sukhvindersatija <86351263+sukhvindersatija@users.noreply.github.com>
2022-10-12 09:18:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,33 +11,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/',(req,res,next)=>{
   res.render('index.ejs')
 });
-// app.post('/', (req, res) => {
-// const data1=req.body.data;
-//  var dataToSend;
-//  console.log(data1)
-//  // spawn new child process to call the python script
-//  const python = spawn('python', ['model.py', data1]);
-//  // collect data from script
-//  python.stdout.on('data', function (data) {
-//   console.log('Pipe data from python script ...');
-//   dataToSend = data.toString();
-//   console.log(dataToSend);
-//  });
-//  // in close event we are sure that stream from child process is closed
-//  python.on('close', (err,code) => {
-//    if(err){
-//      console.log(err);
-//    }
-//  console.log(`child process close all stdio with code ${code}`);
-//  // send data to browser
-//  console.log(dataToSend);
-//  res.send(dataToSend);
-//  // res.render('summary.ejs',{
-//  //   data1:dataToSend
-//  // })
-//  });
-//
-// })
 let {PythonShell}=require('python-shell')
 app.post('/',(req,res,next)=>{
   const data1=req.body.data;
